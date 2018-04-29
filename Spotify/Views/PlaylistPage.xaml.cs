8dc9864dce37fb5e07d671a3ad2794e5bc299037
Clara Schulz-Theißen <Schulz-Theissen@compra.de>
2018-04-29 22:26:01 +0200

    Playlist UI bug fixed.

--- Spotify/Views/PlaylistPage.xaml.cs
+++ Spotify/Views/PlaylistPage.xaml.cs
@@ -1,8 +1,14 @@
 ﻿namespace Spotify.Views
 {
+  using System.ComponentModel;
+
   using Microsoft.Practices.Prism.Mvvm;
+  using Microsoft.Toolkit.Uwp.UI.Animations;
+
+  using Spotify.ViewModels;
 
   using Windows.ApplicationModel;
+  using Windows.UI.Xaml;
   using Windows.UI.Xaml.Controls;
 
   /// <summary>The playlist page.</summary>
@@ -22,6 +28,43 @@
       if (!DesignMode.DesignModeEnabled)
       {
         ViewModelLocator.SetAutoWireViewModel(this, true);
+      }
+
+      this.ViewModel.PropertyChanged += this.ViewModelPropertyChanged;
+
+      this.Unloaded += this.PlaylistPageUnloaded;
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>Gets the view model.</summary>
+    private PlaylistPageViewModel ViewModel
+    {
+      get { return this.DataContext as PlaylistPageViewModel; }
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>Handles the Unloaded event of the PlaylistPage control.</summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="Windows.UI.Xaml.RoutedEventArgs" /> instance containing the event data.</param>
+    private void PlaylistPageUnloaded(object sender, RoutedEventArgs e)
+    {
+      this.ViewModel.PropertyChanged -= this.ViewModelPropertyChanged;
+    }
+
+    /// <summary>Handles the PropertyChanged event of the ViewModel control.</summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="System.ComponentModel.PropertyChangedEventArgs" /> instance containing the event data.</param>
+    private async void ViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+      if (e.PropertyName == "Image" && this.ViewModel.Image != null)
+      {
+        await this.BlurImage.Blur(20).StartAsync();
       }
     }
 
